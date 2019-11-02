fa602a18435fd1ce34b5ca3924d2a4b527bd3dbd
Mystic Quest <l1skou@gmail.com>
2019-11-02 23:51:40 +0200

    chained route handlers

--- app.js
+++ app.js
@@ -40,6 +40,44 @@ const Article = mongoose.model("Article", articleSchema);
 
 //------------------------------------ROUTING-----------------------------
 
+//chaining routes
+
+app.route("/articles")
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function(req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.title
+    });
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Added new article!")
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function(req, res) {
+    Article.deleteMany({}, function(err) {
+      if (!err) {
+        res.send("Deleted!")
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+
+
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
